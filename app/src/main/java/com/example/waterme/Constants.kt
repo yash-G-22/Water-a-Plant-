1662d5bce9aaaa2b875345e9c259a651b9f06f5c
Yash Gupta <gupta58901@gmail.com>
2024-03-24 20:19:27 +0530

    third commit

---
+++ app/src/main/java/com/example/waterme/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.waterme
+val VERBOSE_NOTIFICATION_CHANNEL_NAME: CharSequence = "Verbose WorkManager Notifications"
+const val VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION = "Shows notifications whenever work starts"
+val NOTIFICATION_TITLE: CharSequence = "Water me!"
+const val CHANNEL_ID = "VERBOSE_NOTIFICATION"
+const val NOTIFICATION_ID = 1
+const val REQUEST_CODE = 0
+const val FIVE_SECONDS: Long = 5
+const val ONE_DAY: Long = 1
+const val SEVEN_DAYS: Long = 7
+const val THIRTY_DAYS: Long = 30
